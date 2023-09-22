cfb1d964828340bb09f5dd5c8166ae6fcd88c703
Youssef ( <yuossefbakier@gmail.com>
2023-09-22 16:32:30 -0700

    Add File 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { '/tmp/school':
-  ensure => 'present',
-  content => 'I love Puppet',
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744'
-}
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
