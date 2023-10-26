ee2966a148ea32324aebd76b43c7363cb2064b59
Yousef <yuossefbakier@gmail.com>
2023-10-26 20:03:35 +0300

    Fix Code Style

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,7 +10,11 @@ URL = "https://jsonplaceholder.typicode.com"
 if __name__ == '__main__':
     users = requests.get(f"{URL}/users/").json()
     with open('todo_all_employees.json', 'w', newline='') as f:
-        data = {user.get('id'): [{"username": user.get('username'), "task": task.get('title'), "completed": task.get('completed')}
-                          for task in requests.get(f'{URL}/todos', params={"userId": user.get('id')}).json()]
-                          for user in users}
+        data = {user.get('id'): [{"username": user.get('username'),
+                                  "task": task.get('title'),
+                                  "completed": task.get('completed')}
+                                 for task in requests.get
+                                 (f'{URL}/todos',
+                                  params={"userId": user.get('id')}).json()]
+                for user in users}
         json.dump(data, f)
