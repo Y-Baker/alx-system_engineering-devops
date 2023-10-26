db38c1483109dd586cb8595a9fc7787a69dec682
Yousef <yuossefbakier@gmail.com>
2023-10-26 19:18:21 +0300

    Fix username not name

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,10 +11,9 @@ if __name__ == '__main__':
     if len(argv) > 1:
         id = int(argv[1])
         url_user = f"{URL}/users/{id}"
-        emp_name = requests.get(url=url_user).json().get('name')
+        emp_name = requests.get(url=url_user).json().get('username')
         tasks_resp = requests.get(f"{URL}/todos/").json()
         tasks = list(filter(lambda _: _.get('userId') == id, tasks_resp))
-        # tasks_complete = list(filter(lambda _: _.get('completed'), tasks))
 
     with open(f'{id}.csv', 'w', newline="") as f:
         writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
